42b8a983ba35a2011dee0ec24d2e3450e3875325
shaikhimran1006 <imran1006shaikh@gmail.com>
2025-10-28 10:25:48 +0530

    Initial commit

---
+++ App.java
@@ -0,0 +1,11 @@
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI/CD Pipeline!");
+        System.out.println("Application running successfully!");
+    }
+
+    public String getMessage() {
+        return "Hello World";
+    }
+}
